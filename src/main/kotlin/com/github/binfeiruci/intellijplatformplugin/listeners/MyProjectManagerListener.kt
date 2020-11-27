30c0515a78b2a6d6568f043db4eab6a5df1201f7
GitHub Action <action@github.com>
2020-11-27 05:27:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/binfeiruci/intellijplatformplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/binfeiruci/intellijplatformplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.binfeiruci.intellijplatformplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.binfeiruci.intellijplatformplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/binfeiruci/intellijplatformplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.binfeiruci.intellijplatformplugin.services
+
+import com.github.binfeiruci.intellijplatformplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
